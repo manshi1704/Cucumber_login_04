48c421652f2aa1606f320830be086377d00c10a3
training_d4.05.02 <training_d4.05.02@accenture.com>
2020-01-29 15:05:01 +0530

    comments

--- src/main/java/demo1/Testrunner.java
+++ src/main/java/demo1/Testrunner.java
@@ -8,8 +8,8 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features="Feature12"
+@CucumberOptions(plugin={"json:target/cucumber.json"}
+		,features="Feature12"
 		,glue= {"step_Definition"})
 
 public class Testrunner {
